d2865eca2e7e31e121da63d5e1b0f43be7dd1155
Barath Arivazhagan <barathmacdec91@gmail.com>
2017-09-09 19:06:03 +0530

    code commit initial commit

---
+++ src/main/java/com/barath/app/SpringSocialFacebookAppApplication.java
@@ -0,0 +1,12 @@
+package com.barath.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSocialFacebookAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSocialFacebookAppApplication.class, args);
+	}
+}
