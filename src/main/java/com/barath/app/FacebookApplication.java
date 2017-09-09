a97afc6eadbdca1ee8d6ea96247dcd9402db9b4d
Barath Arivazhagan <barathmacdec91@gmail.com>
2017-09-09 19:08:52 +0530

    code commit

--- src/main/java/com/barath/app/FacebookApplication.java
+++ src/main/java/com/barath/app/FacebookApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringSocialFacebookAppApplication {
+public class FacebookApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringSocialFacebookAppApplication.class, args);
+		SpringApplication.run(FacebookApplication.class, args);
 	}
 }
